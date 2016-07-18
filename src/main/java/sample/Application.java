35b34e705c5acb7571ae8eb131ec684a71a6f80e
fumi <fumi@jp>
2016-07-19 01:40:11 +0900

    testコミット　eclipseから

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("aaaaa");
         SpringApplication.run(Application.class, args);
     }
 }

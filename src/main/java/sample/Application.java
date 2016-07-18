2c80136b17efe14cb25f76bacceb5731e1c75d2a
f-konashi <f-konashi@keyportsolutions.com>
2016-07-19 01:51:12 +0900

    Update Application.java
    web

--- src/main/java/sample/Application.java
+++ src/main/java/sample/Application.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("aaaaa");
+        System.out.println("aaaaacccc");
         SpringApplication.run(Application.class, args);
     }
 }
